7313e54a224be7cfe321448a2d8a134f71fb428e
sebah1989 <sebah1989@gmail.com>
2014-06-10 17:34:39 +0200

    user can see only his own bookcase

--- app/controllers/bookcases_controller.rb
+++ app/controllers/bookcases_controller.rb
@@ -1,6 +1,13 @@
 class BookcasesController < ApplicationController
   expose(:bookcase) 
   expose(:bookcase_books) { bookcase.books.order(:title).page params[:page] }
+  before_action :bookcase_owner?
   def show
+  end
+  private
+  def bookcase_owner?
+    if bookcase.id != current_user.bookcase.id
+      redirect_to root_path
+    end
   end
 end
